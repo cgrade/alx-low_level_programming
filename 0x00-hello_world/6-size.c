b7c071b4b3a9c96bdf6eadffbe32805547dbfc76
cgrade <gradeisageek@gmail.com>
2022-09-08 13:16:34 -0700

    A Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,8 +10,8 @@ int main(void)
 	int B = 10;
 	long long int C = 64;
 
-	printf("Size of a char: %ld", sizeof(A));
-	printf("Size of an int: %d", sizeof(B));
-	printf("Size of a long long int: %ld", sizeof(C));
+	printf("Size of a char: %ld\n", sizeof(A));
+	printf("Size of an int: %d\n", sizeof(B));
+	printf("Size of a long long int: %ld\n", sizeof(C));
 	return (0);
 	}	
