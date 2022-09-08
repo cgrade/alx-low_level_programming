1b442992f32133aca9380cdff1be47e2e0f00f53
cgrade <gradeisageek@gmail.com>
2022-09-08 13:09:04 -0700

    A Commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char A = 'F';
+	int B = 10;
+	long long int C = 64;
+
+	printf("Size of a char: %c", sizeof(A));
+	printf("Size of an int: %ld", sizeof(B));
+	printf("Size of a long long int: %ld", sizeof(C);
+	return (0);
+	}	
