7ab1a7bbed0397687d0cf4790df885e9654cd503
cgrade <gradeisageek@gmail.com>
2022-09-08 13:36:35 -0700

    A Commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,12 @@ int main(void)
 	int B = 10;
 	long int G = 4;
 	long long int C = 64;
+	float F = 2.5f;
 
-	printf("Size of a char: %ld\n", sizeof(A),"bytes(s)");
-	printf("Size of an int: %d\n", sizeof(B),"byte(s)");
-	printf("Size of a long int: %ld\n", sizeof(G),"bytes(s)");
-	printf("Size of a long long int: %ld\n", sizeof(C),"bytes(s)");
+	printf("Size of a char: %ld\n", sizeof(A) "bytes(s)");
+	printf("Size of an int: %d\n", sizeof(B) "byte(s)");
+	printf("Size of a long int: %ld\n", sizeof(G) "bytes(s)");
+	printf("Size of a long long int: %ld\n", sizeof(C) "bytes(s)");
+	printf("Size of a float: %f\n", sizeof(F) "bytes(s)");
 	return (0);
 	}	
