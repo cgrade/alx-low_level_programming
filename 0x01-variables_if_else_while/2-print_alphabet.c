82cd2bf9b532e13e058c04ae922ea907a1b46ce8
cgrade <gradeisageek@gmail.com>
2022-09-10 17:06:28 -0700

    Task 1 added

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void);
+{
+	int ch;
+	for(ch = 0;ch <= 'z';ch++);
+	{
+		putchar(ch);
+	}
+	return (0);
+}
