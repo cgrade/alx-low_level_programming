0d492715ccea26173894402c2fbf09e40ef365ba
cgrade <gradeisageek@gmail.com>
2022-09-10 18:40:04 -0700

    Task 7 added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 'a'; a--)
+	{
+	putchar(a);
+	}
+	{
+	putchar('\n');
+	}
+	return (0);
+}
