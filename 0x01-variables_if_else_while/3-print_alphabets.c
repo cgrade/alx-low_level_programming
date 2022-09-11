db78476143e72bd132559fc888fc75b85cd1e46c
cgrade <gradeisageek@gmail.com>
2022-09-10 17:48:09 -0700

    Task 3 added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sm;
+	int bg;
+
+	for (sm = 97; sm <= 'z'; sm++)
+	{
+		putchar(sm);
+	}
+	for (bg = 101; bg <= 'Z'; bg++)
+	{
+		putchar(bg);
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
+
