c2103d09fc70591474f4e481999d559661abe743
cgrade <gradeisageek@gmail.com>
2022-09-13 09:16:37 -0700

    Task 2  added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main -  prints alphabet in lower case times 10 followed by a new line
+ */
+void print_alpohabet_x10(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		int j;
+		for (j = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar(i);
+	}
+}
