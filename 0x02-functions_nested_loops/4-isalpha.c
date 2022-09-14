c4a2bb45a6b31a61d94aada6cabf2a9c9c231781
cgrade <gradeisageek@gmail.com>
2022-09-14 01:05:05 -0700

    Task 4 added

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,11 @@
 #include "main.h"
 /**
  * _isalpha.c - A program that checks for alphebetic character
- * @c: is th the integer argument
+ * 
+ * @c: is the argument of the function
+ * 
  * Return: return 0 or 1 depending on the condition
+ *
  */
 int _isalpha(int c)
 {
