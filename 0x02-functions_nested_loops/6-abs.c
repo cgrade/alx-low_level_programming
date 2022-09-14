0ae0b37ef872d7e815be5e6702ab40c36b2f45dd
cgrade <gradeisageek@gmail.com>
2022-09-14 01:44:40 -0700

    Task 5 added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: argument passed must be an type int
+ * Return: the value of the computation
+ */
+int _abs(int i)
+{
+	int ret;
+
+	ret = _abs(i);
+	_putchar(i);
+	return (ret);
+}
