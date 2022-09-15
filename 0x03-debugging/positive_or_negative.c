7466af5d477703074638e67fd4872d35bbf5e476
cgrade <gradeisageek@gmail.com>
2022-09-15 04:38:53 -0700

    a commit

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,23 +9,23 @@
  * Return: Always 0 (Success)
  */
 
-int positive_or_negative(int i) 
+int positive_or_negative()
 {
-	int i;
+	int n;
 
 	srand(time(0));
-	i = rand() - RAND_MAX / 2;
-	if (i < 0)
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
 	{
-		printf("%d is negative\n", i);
+		printf("%d is negative\n", n);
 	}
-	if (i == 0)
+	if (n == 0)
 	{
-		printf("%d is zero\n", i);
+		printf("%d is zero\n", n);
 	}
-	if (i > 0)
+	if (n > 0)
 	{
-		printf("%d is positive\n", i);
+		printf("%d is positive\n", n);
 	}
 	return (0);
 }
