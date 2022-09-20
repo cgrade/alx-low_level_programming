ea38b4c2bca14f20a75f3e01ddcbbe1536e1488e
cgrade <gradeisageek@gmail.com>
2022-09-19 19:45:30 -0700

    tasks added

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -1,20 +1,15 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
-	int a;
-	int b;
-	
-	a = 98;
-	b = 42;
-	printf("a=%d, b=%d\n", a, b);
-	swap_int(&a, &b);
-	printf("a=%d, b=%d\n", a, b);
+	char *str;
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
 	return (0);
 }
