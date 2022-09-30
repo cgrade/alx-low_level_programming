48a9922394d36ee8b28dbb5f439aa19386223352
cgrade <gradeisageek@gmail.com>
2022-09-30 09:16:12 -0700

    Hot fix on task 0 again

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * main -  A program tht print it's name followed by a new line
- * @argc: a variable that stores the number of arguments passed
- * @argv: an Array variable that stores all the arguments passed
- * Return: 0 (Always Success)
- */
-
+* main -> this is a function to print its name
+* @argc: argc parameter
+* @argv: an array of a command listed
+* Return: 0
+*/
 int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("the program's name is %s\n", *argv);
+	printf("%s\n", *argv);
 	return (0);
 }
