85da75babf18fe902e23a24d1fce1865a166160c
cgrade <gradeisageek@gmail.com>
2022-09-30 09:08:21 -0700

    Task 0 added( printing the name of the program executed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -  A program tht print it's name followed by a new line
+ * @argc: a variable that stores the number of arguments passed
+ * @argv: an Array variable that stores all the arguments passed
+ * Return: 0 (Always Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("the program's name is %s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,4 @@
+#argc_argv#
+
+this directory talks about 'argc' and 'argv' and projects/exercises that has to do with the application of these concept in C programming and compilation
+
