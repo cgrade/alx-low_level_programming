734aa8da6b714db78eaaa64d47ef7fb26db27a57
cgrade <gradeisageek@gmail.com>
2022-09-30 19:10:58 -0700

    Task added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A program that print's number of arguments passed.
+ * @argc: int variable that records the number of arg passed
+ * @argv: char variable that holds the args passed
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
