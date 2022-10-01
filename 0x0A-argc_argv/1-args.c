5cc2384f32b33b0eb0291f64192adb8cfe0e3842
cgrade <gradeisageek@gmail.com>
2022-09-30 19:22:03 -0700

    Task added

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: char variable that holds the args passed
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
