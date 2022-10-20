b43c9fa51d2b284dbd636e7acbd7385ba12ab0db
cgrade <gradeisageek@gmail.com>
2022-10-20 15:06:47 -0700

    Task Added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,18 +1,14 @@
-#include <stdio.h>
-
+#include "function_pointers.h"
+#include<stdlib.h>
 /**
- * print_name -> a function that prints a name
- *
- * @name: parameter 1
- * @f: parameter function
+ * print_name - prints a name
+ * @name: person's name
+ * @f: pointer to a function
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-	{
-		if (name != NULL)
-		{
-			(*f)(name);
-		}
-	}
+if (f != NULL)
+f(name);
+
 }
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+# A readme file for singly linked list, this directory contains project tasks for the singly linked list concept in C programming language
