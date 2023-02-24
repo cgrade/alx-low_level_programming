3269052691347448b6c2e2176fa38c652afd873e
cgrade <gradeisageek@gmail.com>
2023-02-24 15:03:37 +0100

    task 5 added

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -16,11 +16,13 @@ def num_water_neighbors(grid, i, j):
         num += 1
     return num
 
+
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j]:
-                perimeter += num_water_neighbors(grid, i, j) 
+                perimeter += num_water_neighbors(grid, i, j)
     return perimeter
